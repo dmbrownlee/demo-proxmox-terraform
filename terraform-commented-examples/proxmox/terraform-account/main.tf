ae63b1bf5527dce9284ed63c73275147fcb3c13d
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-01-19 02:51:04 +0000

    Initial terraform account example.

---
+++ terraform-commented-examples/proxmox/terraform-account/main.tf
@@ -0,0 +1,91 @@
+terraform {
+  required_version = "~> 1.6.1"
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.43.2"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
+    }
+  }
+}
+
+#-- Providers --------------------------------------
+provider "proxmox" {
+  endpoint  = var.endpoint
+  username  = var.rootaccount
+  password  = var.rootpassword
+  insecure  = true
+  ssh {
+    agent = true
+  }
+}
+
+#-- Variables --------------------------------------
+variable "endpoint" {
+  description = "URL of the Proxmox cluster API"
+  type        = string
+}
+
+variable "rootaccount" {
+  description = "Default Proxmox administration account"
+  type        = string
+  default     = "root@pam"
+}
+
+variable "rootpassword" {
+  description = "Password for default Proxmox administration account"
+  type        = string
+  sensitive   = true
+}
+
+variable "tfaccount" {
+  description = "Terraform administration account"
+  type        = string
+  default     = "terraform@pve"
+}
+
+#-- Resources --------------------------------------
+resource "proxmox_virtual_environment_role" "role_terraform" {
+  role_id = "TERRAFORM"
+
+  privileges = [
+    "Datastore.Allocate",
+    "Datastore.AllocateSpace",
+    "Datastore.AllocateTemplate",
+    "Datastore.Audit",
+    "Pool.Allocate",
+    "Sys.Audit",
+    "Sys.Console",
+    "Sys.Modify",
+    "VM.Allocate",
+    "VM.Audit",
+    "VM.Clone",
+    "VM.Config.CDROM",
+    "VM.Config.Cloudinit",
+    "VM.Config.CPU",
+    "VM.Config.Disk",
+    "VM.Config.HWType",
+    "VM.Config.Memory",
+    "VM.Config.Network",
+    "VM.Config.Options",
+    "VM.Migrate",
+    "VM.Monitor",
+    "VM.PowerMgmt",
+    "SDN.Use"
+  ]
+}
+
+resource "proxmox_virtual_environment_user" "user_terraform" {
+  acl {
+    path      = "/"
+    propagate = true
+    role_id   = proxmox_virtual_environment_role.role_terraform.role_id
+  }
+  enabled = true
+  groups  = []
+  user_id = var.tfaccount
+}
+
