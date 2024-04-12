11cc595ccd8ee3c05c448d05caac4353566892ab
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-04-12 05:00:29 +0000

    Remove old commented out code.

--- lab-k3s/data.tf
+++ lab-k3s/data.tf
@@ -4,10 +4,6 @@ data "proxmox_virtual_environment_vms" "cloud_init_template" {
   tags = ["cloud_init_template"]
 }
 
-data "local_sensitive_file" "ci_ssh_private_key_file" {
-  filename = pathexpand("${var.ssh_keystore}/${var.ci_user}")
-}
-
 data "local_file" "ci_ssh_public_key_file" {
   filename = pathexpand("${var.ssh_keystore}/${var.ci_user}.pub")
 }
--- lab-yunohost/data.tf
+++ lab-yunohost/data.tf
@@ -4,10 +4,6 @@ data "proxmox_virtual_environment_vms" "cloud_init_template" {
   tags = ["cloud_init_template"]
 }
 
-/* /1* data "local_sensitive_file" "ci_ssh_private_key_file" { *1/ */
-/* /1*   filename = pathexpand("${var.ssh_keystore}/${var.ci_user}") *1/ */
-/* /1* } *1/ */
-
 data "local_file" "ci_ssh_public_key_file" {
   filename = pathexpand("${var.ssh_keystore}/${var.ci_user}.pub")
 }
