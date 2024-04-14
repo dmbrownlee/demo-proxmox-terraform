9976994172df5fa01cd5e2dab1dd441f51c651b6
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-04-14 05:03:06 +0000

    Add unconfigured Grafana.

--- lab-k3s/k3s-ansible.tf
+++ lab-k3s/k3s-ansible.tf
@@ -206,3 +206,30 @@ resource "ansible_playbook" "k3s_prometheus" {
 output "playbook_output_k3s_prometheus" {
   value = var.want_ansible_output ? ansible_playbook.k3s_prometheus : null
 }
+
+
+###############################################################################
+##
+##  Grafana data visualization
+##
+###############################################################################
+resource "ansible_playbook" "k3s_grafana" {
+  playbook                = "ansible/playbook-grafana.yml"
+  name                    = "localhost"
+  replayable              = var.ansible_replayable
+  ignore_playbook_failure = true
+  extra_vars = {
+    ansible_hostname          = "localhost"
+    ansible_connection        = "local"
+    private_key               = var.ssh_private_key_files[var.ci_user]
+    ansible_ssh_user          = var.ci_user
+    k3s_local_kubeconfig_path = var.k3s_local_kubeconfig_path
+  }
+  depends_on = [
+    resource.ansible_playbook.k3s_longhorn,
+  ]
+}
+
+output "playbook_output_k3s_grafana" {
+  value = var.want_ansible_output ? ansible_playbook.k3s_grafana : null
+}
