69def3988b4baa55ee604dc99b276d7271949e07
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-11-27 19:54:36 +0000

    Add comments and a default value to variables.

--- stage2-vm-templates/variables.tf
+++ stage2-vm-templates/variables.tf
@@ -1,3 +1,9 @@
+#===========================================================
+# These variables are used by the Terraform Proxmox provider
+#
+# Rather than editng the default values here, you can
+# override them in terraform.tfvars
+#===========================================================
 variable "api_token" {
   description = "Proxmox API token"
   type        = string
@@ -6,4 +12,5 @@ variable "api_token" {
 variable "endpoint" {
   description = "URL of the Proxmox cluster API"
   type        = string
+  default     = "https://pve1.example.com:8006/"
 }
