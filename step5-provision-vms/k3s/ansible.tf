a4658227ac166378307264b75644acf46eb264bb
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2024-12-04 23:47:56 +0000

    Use ansible_domain instead of defining another variable.

--- step5-provision-vms/k3s/ansible.tf
+++ step5-provision-vms/k3s/ansible.tf
@@ -25,11 +25,6 @@ variable "k3s_local_kubeconfig_path" {
   type        = string
 }
 
-variable "k3s_vip_domain" {
-  description = "The site domain"
-  type        = string
-}
-
 
 ###############################################################################
 ###############################################################################
