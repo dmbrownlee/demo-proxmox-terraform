2d7f1f0d82c9915a0a988a67d826ba9f3ccc04cf
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2026-03-25 22:13:02 +0000

    Add support for TTL in static-dns project.

--- step3-project-vms/static-dns/dns.tf
+++ step3-project-vms/static-dns/dns.tf
@@ -36,7 +36,8 @@ variable "static_a_records" {
   description = "List of static A record objects"
   type = list(object({
     name      = string,
-    addresses = list(string)
+    addresses = list(string),
+    ttl       = number
   }))
   default = []
 }
@@ -53,5 +54,5 @@ resource "dns_a_record_set" "static_a_records" {
   zone      = "${var.site_domain}."
   name      = var.static_a_records[count.index].name
   addresses = var.static_a_records[count.index].addresses
-  ttl       = 300
+  ttl       = var.static_a_records[count.index].ttl
 }
