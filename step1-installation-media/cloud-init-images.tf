26687f3616d0dd64dcc79c03dd0e74c0909883e8
D. M. Brownlee <dmbrownlee@users.noreply.github.com>
2026-03-25 22:20:17 +0000

    Add support for import installation media.

--- step1-installation-media/cloud-init-images.tf
+++ step1-installation-media/cloud-init-images.tf
@@ -1,7 +1,14 @@
+variable "cloud_init_storage" {
+  type = object({
+    node_name = string,
+    datastore_id = string
+  })
+}
+
 variable "iso_storage" {
   type = object({
-    node = string,
-    name = string
+    node_name = string,
+    datastore_id = string
   })
 }
 
@@ -13,12 +20,30 @@ variable "cloud_init_images" {
   }))
 }
 
+variable "iso_media" {
+  description = "Map of cloud-init image files to download"
+  type = map(object({
+    file_name = string,
+    url       = string
+  }))
+}
+
 resource "proxmox_virtual_environment_download_file" "cloud_init_images" {
   for_each     = var.cloud_init_images
+  content_type = "import"
+  overwrite    = true
+  datastore_id = var.cloud_init_storage.datastore_id
+  file_name    = each.value.file_name
+  node_name    = var.cloud_init_storage.node_name
+  url          = each.value.url
+}
+
+resource "proxmox_virtual_environment_download_file" "iso_media" {
+  for_each     = var.iso_media
   content_type = "iso"
   overwrite    = true
-  datastore_id = var.iso_storage.name
+  datastore_id = var.iso_storage.datastore_id
   file_name    = each.value.file_name
-  node_name    = var.iso_storage.node
+  node_name    = var.iso_storage.node_name
   url          = each.value.url
 }
